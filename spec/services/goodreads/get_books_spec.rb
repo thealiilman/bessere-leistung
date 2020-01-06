50f772ee825be654f4e538fd3ab2b296c6532057
thealiilman <aliilman532@gmail.com>
2020-01-06 21:04:32 +0800

    Written spec for .run method with an example for all expectations.

--- spec/services/goodreads/get_books_spec.rb
+++ spec/services/goodreads/get_books_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 describe Goodreads::GetBooks, type: :service do
+  describe '.run' do
+    let(:titles) do
+      ['And Then There Were None', 'Far from the Madding Crowd', 'This Was a Man']
+    end
+    let(:authors) { ['Agatha Christie', 'Tom Hardy', 'Jeffrey Archer'] }
+
+    subject { described_class.run }
+
+    it do
+      expect(subject.length).to eq(3)
+      expect(subject.pluck(:author)).to eq(authors)
+      expect(subject.pluck(:title)).to eq(titles)
+    end
+  end
 end
