530b95df114c98117926979a91e64a1b05d2d8e7
thealiilman <aliilman532@gmail.com>
2020-01-06 21:00:03 +0800

    Created a service called Goodreads::GetBooks.

---
+++ app/services/goodreads/get_books.rb
@@ -0,0 +1,26 @@
+module Goodreads
+  class GetBooks
+    def self.run
+      new.run
+    end
+
+    def run
+      sleep(2)
+
+      [
+        {
+          title: 'And Then There Were None',
+          author: 'Agatha Christie'
+        },
+        {
+          title: 'Far from the Madding Crowd',
+          author: 'Tom Hardy'
+        },
+        {
+          title: 'This Was a Man',
+          author: 'Jeffrey Archer'
+        }
+      ]
+    end
+  end
+end
