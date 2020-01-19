f829b11c72de4965500bb52e10d5490eec141a5e
thealiilman <aliilman532@gmail.com>
2020-01-19 17:07:43 +0800

    Written spec for .run method for let! example.

--- spec/services/books/update_availability_spec.rb
+++ spec/services/books/update_availability_spec.rb
@@ -2,5 +2,21 @@ require 'rails_helper'
 
 describe Books::UpdateAvailability, type: :service do
   describe '.run' do
+    let!(:book) do
+      # An expensive process
+      sleep(2)
+
+      create(
+        :book,
+        title: 'This Was A Man',
+        available: true,
+        author: create(:author, name: 'Jeffrey Archer')
+      )
+    end
+
+    subject { described_class.run(book) }
+
+    it { expect(subject.success?).to be true }
+    it { expect(subject.book.available).to be false }
   end
 end
