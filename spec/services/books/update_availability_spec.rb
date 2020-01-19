0793b1c8e0c8653458ac1c6a891700a26cb5d18c
thealiilman <aliilman532@gmail.com>
2020-01-19 18:17:13 +0800

    Written spec for .run method in context of let! and a single example.

--- spec/services/books/update_availability_spec.rb
+++ spec/services/books/update_availability_spec.rb
@@ -21,5 +21,27 @@ describe Books::UpdateAvailability, type: :service do
       it { expect(subject.success?).to be true }
       it { expect(subject.book.available).to be false }
     end
+
+    # On average, it takes 7 seconds.
+    context 'let! and a single example' do
+      let(:book) do
+        # An expensive process
+        sleep(2)
+
+        create(
+          :book,
+          title: 'This Was A Man',
+          available: true,
+          author: create(:author, name: 'Jeffrey Archer')
+        )
+      end
+
+      subject { described_class.run(book) }
+
+      it do
+        expect(subject.success?).to be true
+        expect(subject.book.available).to be false
+      end
+    end
   end
 end
