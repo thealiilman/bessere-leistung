81fd0dba3a453ac20bbfb2c0436640dfd9315cf2
thealiilman <aliilman532@gmail.com>
2020-01-23 18:44:52 +0800

    Added context to specify examples (it) tests.

--- spec/services/books/update_availability_spec.rb
+++ spec/services/books/update_availability_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 describe Books::UpdateAvailability, type: :service do
   describe '.run' do
-    # On average, it takes 14 seconds.
-    context 'multiple examples' do
+    subject { described_class.run(book) }
+
+    context 'examples (it)' do
       let(:book) do
         # An expensive process
         sleep(2)
@@ -16,31 +17,18 @@ describe Books::UpdateAvailability, type: :service do
         )
       end
 
-      subject { described_class.run(book) }
-
-      it { expect(subject.success?).to be true }
-      it { expect(subject.book.available).to be false }
-    end
-
-    # On average, it takes 7 seconds.
-    context 'a single example' do
-      let(:book) do
-        # An expensive process
-        sleep(2)
-
-        create(
-          :book,
-          title: 'This Was A Man',
-          available: true,
-          author: create(:author, name: 'Jeffrey Archer')
-        )
+      # On average, it takes 14 seconds.
+      context 'multiple examples' do
+        it { expect(subject.success?).to be true }
+        it { expect(subject.book.available).to be false }
       end
 
-      subject { described_class.run(book) }
-
-      it do
-        expect(subject.success?).to be true
-        expect(subject.book.available).to be false
+      # On average, it takes 7 seconds.
+      context 'a single example' do
+        it do
+          expect(subject.success?).to be true
+          expect(subject.book.available).to be false
+        end
       end
     end
   end
